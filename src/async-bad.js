2c57642dd0dc7df3471b90ecc775eff22a91b2fc
Xinyun Zhou <me@xyzhou.com>
2018-03-31 11:52:02 +1100

    adding async example

---
+++ src/async-bad.js
@@ -0,0 +1,19 @@
+const throwError = () => {
+  throw new Error("throwing error object")
+}
+
+const promiseInvoker = () => {
+  return new Promise((resolve, reject) => {
+    resolve(throwError())
+  })
+}
+
+try {
+  (async () => {
+    await promiseInvoker()
+  })()
+} catch (e) {
+  console.log('E.MESSAGE => ' + e.message)
+  console.log('E.NAME => ' + e.name)
+  console.log('E.STACK => ' + e.stack)
+}
